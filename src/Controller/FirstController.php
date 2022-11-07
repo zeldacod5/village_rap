1ba51d7b416676f83828f21a50811472a49016fe
zeldacod5 <loucas.herault@laposte.net>
2022-11-07 11:55:12 +0100

    lundi matin pull

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'app_first')]
+    public function index(): Response
+    {
+        return $this->render('first/index.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        return $this->render('first/home.html.twig');
+    }
+}
