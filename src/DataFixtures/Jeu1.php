23a8c05b58a2256eb1e9b1bb89536fa0451d0e7c
zeldacod5 <loucas.herault@laposte.net>
2022-11-29 13:27:02 +0100

    commit des fixtures

--- src/DataFixtures/Jeu1.php
+++ src/DataFixtures/Jeu1.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Subcategory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,10 +12,30 @@ class Jeu1 extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+        $cat1 = new Category();
+        $cat1->setName("RAP Français");
+        $cat1->setPicture("image1.jpg");
+        $manager->persist($cat1);
+
+
+        $souscat1 = new Subcategory();
+        $souscat1->setName("Drill");
+        $manager->persist($souscat1);
+        $souscat1->setCategory($cat1);
+
+        $souscat2 = new Subcategory();
+        $souscat2->setName("Drill 22");
+        $manager->persist($souscat2);
+        $souscat2->setCategory($cat1);
+
+
+
         $produit1 = new Product();
         $produit1->setName("MC SOLAAR - Cinquième As");
         $produit1->setLongLib("Album référence de MC Solaar");
         $manager->persist($produit1);
+        $produit1->addSubcategory($souscat1);
 
         $manager->flush();
     }
