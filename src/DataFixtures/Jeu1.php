ecb1a651c4e5a90102428bbeb67dff1b3a7cf9ae
zeldacod5 <loucas.herault@laposte.net>
2022-12-16 02:27:28 +0100

    modification page produit

--- src/DataFixtures/Jeu1.php
+++ src/DataFixtures/Jeu1.php
@@ -71,7 +71,8 @@ class Jeu1 extends Fixture
 
 
         $produit1 = new Product();
-        $produit1->setName("MC SOLAAR - Cinquième As");
+        $produit1->setName("Cinquième As");
+        $produit1->setArtistname("MC SOLAAR");
         $produit1->setLongLib("Album référence de MC Solaar");
         $produit1->setShortLib("sortie le 13 mars 2001..");
         $produit1->setPrice(28);
@@ -80,7 +81,8 @@ class Jeu1 extends Fixture
         $produit1->setSubcategory($souscat1);
 
         $produit2 = new Product();
-        $produit2->setName("JAZZY BAZZ - Memoria");
+        $produit2->setName("Memoria");
+        $produit2->setArtistname("JAZZY BAZZ");
         $produit2->setLongLib("Troisième album de Jazzy Bazz");
         $produit2->setShortLib("sortie le 21 janvier 2022..");
         $produit2->setPrice(19,99);
@@ -89,16 +91,18 @@ class Jeu1 extends Fixture
         $produit2->setSubcategory($souscat4);
 
         $produit3 = new Product();
-        $produit3->setName("LAYLOW - L'étrange Histoire de Mr.Anderson");
+        $produit3->setName("L'étrange Histoire de Mr.Anderson");
+        $produit3->setArtistname("LAYLOW");
         $produit3->setLongLib("Deuxième album studio du rappeur!");
         $produit3->setShortLib("sortie le 16 juillet 2021..");
         $produit3->setPrice(19,99);
-        $produit3->setPicture("laylow.jpeg");
+        $produit3->setPicture("laylow3.jpg");
         $manager->persist($produit3);
         $produit3->setSubcategory($souscat4);
 
         $produit4 = new Product();
-        $produit4->setName("SAMEER AHMAD - Apaches");
+        $produit4->setName("Apaches");
+        $produit4->setArtistname("SAMEER AHMAD");
         $produit4->setLongLib("Rappeur de Montpellier");
         $produit4->setShortLib("sortie le 12 juin 2019..");
         $produit4->setPrice(22);
@@ -107,7 +111,8 @@ class Jeu1 extends Fixture
         $produit4->setSubcategory($souscat4);
 
         $produit5 = new Product();
-        $produit5->setName("OXMO PUCCINO - Opéra Puccino");
+        $produit5->setName("Opéra Puccino");
+        $produit5->setArtistname("OXMO PUCCINO");
         $produit5->setLongLib("Deuxième album studio du rappeur!");
         $produit5->setShortLib("sortie le 28 avril 1998..");
         $produit5->setPrice(30);
@@ -116,7 +121,8 @@ class Jeu1 extends Fixture
         $produit5->setSubcategory($souscat1);
 
         $produit6 = new Product();
-        $produit6->setName("IDEAL J - Le combat continue");
+        $produit6->setName("Le combat continue");
+        $produit6->setArtistname("IDEAL J");
         $produit6->setLongLib("Album emblèmatique, les débuts du rappeur Kery James");
         $produit6->setShortLib("sortie le 26 octobre 1998..");
         $produit6->setPrice(20);
@@ -125,7 +131,8 @@ class Jeu1 extends Fixture
         $produit6->setSubcategory($souscat1);
 
         $produit7 = new Product();
-        $produit7->setName("IAM - L'école du micro d'argent");
+        $produit7->setName("L'école du micro d'argent");
+        $produit7->setArtistname("IAM");
         $produit7->setLongLib("Album mythique du groupe IAM!");
         $produit7->setShortLib("sortie le 18 mars 1997..");
         $produit7->setPrice(28);
@@ -134,7 +141,8 @@ class Jeu1 extends Fixture
         $produit7->setSubcategory($souscat1);
 
         $produit8 = new Product();
-        $produit8->setName("LUNATIC - Mauvais oeil");
+        $produit8->setName("Mauvais oeil");
+        $produit8->setArtistname("LUNATIC");
         $produit8->setLongLib("Album des deux lunatics, Booba Ali");
         $produit8->setShortLib("sortie le 24 octobre 2000..");
         $produit8->setPrice(22);
@@ -143,7 +151,8 @@ class Jeu1 extends Fixture
         $produit8->setSubcategory($souscat1);
 
         $produit9 = new Product();
-        $produit9->setName("50 CENT - The Massacre");
+        $produit9->setName("The Massacre");
+        $produit9->setArtistname("50 CENT");
         $produit9->setLongLib("Le massacre mec");
         $produit9->setShortLib("sortie le 3 mars 2005 ");
         $produit9->setPrice(19,99);
@@ -152,7 +161,8 @@ class Jeu1 extends Fixture
         $produit9->setSubcategory($souscat2);
 
         $produit10 = new Product();
-        $produit10->setName("EMINEM - The Eminem Show");
+        $produit10->setName("The Eminem Show");
+        $produit10->setArtistname("EMINEM");
         $produit10->setLongLib("Album de dingue des années 2000");
         $produit10->setShortLib("sortie le 26 mai 2001..");
         $produit10->setPrice(19,99);
@@ -161,7 +171,8 @@ class Jeu1 extends Fixture
         $produit10->setSubcategory($souscat2);
 
         $produit11 = new Product();
-        $produit11->setName("NOTORIOUS BIG - Ready to Die");
+        $produit11->setName("Ready to Die");
+        $produit11->setArtistname("NOTORIOUS BIG");
         $produit11->setLongLib("La c'est nimp..");
         $produit11->setShortLib("sortie le 13 septembre 1994");
         $produit11->setPrice(25);
@@ -170,7 +181,8 @@ class Jeu1 extends Fixture
         $produit11->setSubcategory($souscat2);
 
         $produit12 = new Product();
-        $produit12->setName("KENDRICK LAMAR - Good Kid, M.A.A.D. City");
+        $produit12->setName("Good Kid, M.A.A.D. City");
+        $produit12->setArtistname("KENDRICK LAMAR");
         $produit12->setLongLib("La c'est reouf..");
         $produit12->setShortLib("sortie le 22 octobre 2012");
         $produit12->setPrice(19,99);
@@ -179,7 +191,8 @@ class Jeu1 extends Fixture
         $produit12->setSubcategory($souscat5);
 
         $produit13 = new Product();
-        $produit13->setName("KENDRICK LAMAR - Damn");
+        $produit13->setName("Damn");
+        $produit13->setArtistname("KENDRICK LAMAR");
         $produit13->setLongLib("La c'est relou même.");
         $produit13->setShortLib("sortie le 14 avril 2017..");
         $produit13->setPrice(26);
@@ -188,7 +201,8 @@ class Jeu1 extends Fixture
         $produit13->setSubcategory($souscat5);
 
         $produit14 = new Product();
-        $produit14->setName("GREEN MONTANA - Nostalgia+");
+        $produit14->setName("Nostalgia+");
+        $produit14->setArtistname("GREEN MONTANA");
         $produit14->setLongLib("Dernier album de GREEN MONTAIN dans les backs !");
         $produit14->setShortLib("sortie le 15 avril 2022..");
         $produit14->setPrice(22);
@@ -197,11 +211,12 @@ class Jeu1 extends Fixture
         $produit14->setSubcategory($souscat4);
 
         $produit15 = new Product();
-        $produit15->setName("FREEZE CORLEONE & ASHE 22 - RIYAD SADIO");
+        $produit15->setName("RIYAD SADIO");
+        $produit15->setArtistname("FREEZE CORLEONE & ASHE 22");
         $produit15->setLongLib("Sortez les fers et les cagoules ...");
         $produit15->setShortLib("sortie le 4 mars 2022");
         $produit15->setPrice(35);
-        $produit15->setPicture("freezeashe.jpg");
+        $produit15->setPicture("riyadsadio2.jpg");
         $manager->persist($produit15);
         $produit15->setSubcategory($souscat4);
 
